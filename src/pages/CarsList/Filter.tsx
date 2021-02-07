c69fd707cf680fb06c47efd7ed1b8e5bfe56051f
Кирилл Костюшко <kostyushkokirill@gmail.com>
2021-02-07 15:42:40 +0300

    add form styles

--- src/pages/CarsList/Filter.tsx
+++ src/pages/CarsList/Filter.tsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react';
 import Box from '@material-ui/core/Box';
 import InputLabel from '@material-ui/core/InputLabel';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import { FiltersContext } from 'context/filtersDictionary/FiltersProvider';
 import { colors as colorPallette } from 'styles/variables';
@@ -18,6 +21,13 @@ type IFilterComponent = {
   manufacturer: string,
 }
 
+const useStyles = makeStyles(() => ({
+  formControl: {
+    // minWidth: '100%',
+    // height: '25px'
+  },
+}));
+
 const MenuProps = {
   PaperProps: {
     style: {
@@ -31,12 +41,13 @@ const MenuProps = {
 };
 
 const Filter = ({ applyFilters, onFiltersHandle, color, manufacturer }: IFilterComponent) => {
+  const classes = useStyles();
   const { colors, manufacturers } = useContext(FiltersContext);
 
   return (
     <Box component='div' border={`2px solid ${colorPallette.secondary}`} padding='24px' width='100%'>
-      <FormControl>
-        <InputLabel htmlFor="color-select">Color</InputLabel>
+      <Typography>Color</Typography>
+      <FormControl className={classes.formControl} variant='outlined' size='small' fullWidth>
         <Select
           value={color}
           onChange={onFiltersHandle}
@@ -45,6 +56,7 @@ const Filter = ({ applyFilters, onFiltersHandle, color, manufacturer }: IFilterC
             id: 'color-select',
           }}
           MenuProps={MenuProps}
+          variant='outlined'
         >
           {colors.map(color => (
             <MenuItem key={color} value={color}>
@@ -53,7 +65,8 @@ const Filter = ({ applyFilters, onFiltersHandle, color, manufacturer }: IFilterC
           ))}
         </Select>
       </FormControl>
-      <FormControl>
+      <Typography>Manufacturer</Typography>
+      <FormControl className={classes.formControl} variant='outlined' size='small' fullWidth>
         <InputLabel htmlFor="manufacturer-select">Manufacturer</InputLabel>
         <Select
           value={manufacturer}
@@ -63,6 +76,7 @@ const Filter = ({ applyFilters, onFiltersHandle, color, manufacturer }: IFilterC
             id: 'manufacturer-select',
           }}
           MenuProps={MenuProps}
+          variant='outlined'
         >
           {manufacturers.map(manufacturer => (
             <MenuItem key={manufacturer.name} value={manufacturer.name}>
